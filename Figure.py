ceca27d3908422c6bac1811d70c558672a024f45
Nikita <Entvk20071@mail.ru>
2023-11-20 16:20:16 +0300

    рефакторинг и написание документации для Figure.py

--- Figure.py
+++ Figure.py
@@ -1,6 +1,25 @@
-import pygame.transform
+"""Модуль всех фигур.
 
-from const import *
+Импорты сторонних библиотек:
+pygame - в классе каждой из фигур есть функция get_img, которая выдаёт изображение этой фигуры в типе pygame-а,
+
+Импорты модулей проекта:
+const: load_image, cell_size - также требуются для создания изображения в get_img,
+
+
+can_move_parent - декоратор проверяющий может ли на то поле сходить родитель,
+copy - копирование 2 фигур,
+
+Figure - класс-родитель всех фигур,
+Pawn - класс Пешки,
+Knight - класс Коня,
+Bishop - класс Слона,
+Rook - класс Ладьи,
+Queen - класс Королевы,
+King - класс Короля."""
+
+import pygame.transform
+from const import load_image, cell_size
 
 
 def can_move_parent(can_move):
@@ -9,65 +28,79 @@ def can_move_parent(can_move):
             return False
 
         return can_move(*args, *kwargs)
+
     return can_move_new
 
 
-def copy(copy_obj, cls):
-    return cls(copy_obj.row, copy_obj.col, copy_obj.color)
-
-
 class Figure:
+    """Класс-родитель (интерфейс, абстрактный класс) фигур."""
+
     def __init__(self, row, col, color: bool):
+        """Инициализация полей.
+
+        Публичные поля:
+        row - строка, где сейчас стоит фигура
+        col - колонка, где сейчас стоит фигура
+        color - цвет фигуры."""
         self.row = row
         self.col = col
         self.color = color
 
-    def set_position(self, row, col):
+    def set_position(self, row: int, col: int):
+        """Измени координаты фигуры на строку row и колонку col (не рекомендуется вылезать за доску)."""
         self.row = row
         self.col = col
 
     def get_color(self):
+        """Дай цвет фигуры."""
         return self.color
 
     def is_enemy(self, figure):
+        """Проверь, вот эта (figure) фигура для меня враг?"""
         if figure is None:
             return False
         return figure.get_color() != self.get_color()
 
     def can_move(self, board, new_row, new_col):
-        if not(0 <= new_row <= 7 and 0 <= new_col <= 7):
-            return False
+        """Проверь, могу ли я сходить на поле по координатам row, col доски board."""
         move_place = board[new_row][new_col]
-        if move_place is None:
-            return True
-        if move_place.get_color() != self.get_color():
-            return True
-        return False
+        return not (self.is_enemy(move_place) or None)
 
     @staticmethod
     def check_path_empty(board, new_row, new_col):
+        """Проверь, свободен ли путь для хода на координаты new_row, new_col доски board."""
         return True
 
     def char(self):
-        ...
-
-    @staticmethod
-    def get_pos_img_in_sp():
-        return 1
+        """Дай буквенное представление."""
 
     def get_img(self):
+        """Дай представление в виде изображения."""
         return pygame.transform.scale(load_image(f'img.png').
                                       subsurface(200 * (self.get_pos_img_in_sp() - 1),
                                                  200 * int(not self.color),
                                                  200, 200),
                                       (cell_size, cell_size))
 
+    @staticmethod
+    def get_pos_img_in_sp():
+        """Верни порядковый номер среди изображений."""
+        return 1
+
 
 class Pawn(Figure):
+    """Класс пешки."""
+
     def __init__(self, row, col, color):
+        """Расширить инициализацию полей.
+
+        Приватные:
+        _first_move - хранит исходное положение пешки,
+        _direction - хранит направление пешки."""
+
         super(Pawn, self).__init__(row, col, color)
-        self.first_move = row
-        self.direction = 1 if self.color else -1
+        self._first_move = row
+        self._direction = 1 if self.color else -1
 
     def can_move(self, board, row, col):
         move_place = board[row][col]
@@ -78,20 +111,22 @@ class Pawn(Figure):
         return False
 
     def short_move(self, move_place, row, col):
-        """Ход на 1 клетку"""
-        return move_place is None and\
-            self.col == col and self.row + self.direction == row
+        """Передвинь пешку на поле move_place с координатами row col на расстоянии 1 клетки."""
+        return (move_place is None and
+                self.col == col and
+                self.row + self._direction == row)
 
     def big_move(self, move_place, row, col):
-        """Ход на 2 клетки"""
-        return move_place is None and \
-            self.col == col and \
-            self.row == self.first_move and self.row + 2 * self.direction == row
+        """Передвинь пешку на поле move_place с координатами row col на расстоянии 2 клетки."""
+        return (move_place is None and
+                self.col == col and
+                self.row == self._first_move and self.row + 2 * self._direction == row)
 
     def can_eat(self, move_place, row, col):
-        if not self.is_enemy(move_place):
-            return False
-        if self.row == row - self.direction and (self.col + 1 == col or self.col - 1 == col):
+        """Проверь, могу ли я съесть фигуру на поле move_place, которое расположено по координатам row, col."""
+        if (self.is_enemy(move_place) and
+                self.row == row - self._direction and
+                (self.col + 1 == col or self.col - 1 == col)):
             return True
         return False
 
@@ -107,6 +142,8 @@ class Pawn(Figure):
 
 
 class Knight(Figure):
+    """Класс коня."""
+
     # пока не трогаю, но можно сделать через модуль от разницы прошлой и настоящей координаты
     @can_move_parent
     def can_move(self, board, new_row, new_col):
@@ -119,6 +156,7 @@ class Knight(Figure):
                 or (self.row + 2 == new_row and self.col - 1 == new_col)
                 or (self.row - 2 == new_row and self.col - 1 == new_col)):
             return True
+        return False
 
     def char(self):
         return 'N'
@@ -129,11 +167,12 @@ class Knight(Figure):
 
 
 class Bishop(Figure):
+    """Класс слона."""
+
     @can_move_parent
     def can_move(self, board, new_row, new_col):
-        if self.col - self.row == new_col - new_row:
-            return self.check_path_empty(board, new_row, new_col)
-        if self.col + self.row == new_col + new_row:
+        if ((self.col - self.row == new_col - new_row) or
+                (self.col + self.row == new_col + new_row)):
             return self.check_path_empty(board, new_row, new_col)
         return False
 
@@ -155,7 +194,14 @@ class Bishop(Figure):
 
 
 class Rook(Figure):
+    """Класс ладьи."""
+
     def __init__(self, row, col, color: bool):
+        """Расширить инициализацию
+
+        Публичное поле:
+        can_castle - способность ладьи к рокировке, убирается при первом ходе."""
+
         super(Rook, self).__init__(row, col, color)
         self.can_castle = True
 
@@ -165,13 +211,15 @@ class Rook(Figure):
 
     @can_move_parent
     def can_move(self, board, new_row, new_col):
-        if self.row == new_row:
-            return self.check_path_row_empty(board, new_col)
-        if self.col == new_col:
-            return self.check_path_col_empty(board, new_row)
+        if (self.row == new_row) or (self.col == new_col):
+            return self.check_path_empty(board, new_row, new_col)
         return False
 
+    def check_path_empty(self, board, new_row, new_col):
+        return self.check_path_row_empty(board, new_col) or self.check_path_col_empty(board, new_col)
+
     def check_path_col_empty(self, board, new_row):
+        """Проверь, свободен ли путь по горизонтали под номером new_row доски board."""
         direction = 1 if self.row - new_row < 0 else -1
         for i in range(1, abs(self.row - new_row)):
             if board[self.row + i * direction][self.col] is not None:
@@ -179,6 +227,7 @@ class Rook(Figure):
         return True
 
     def check_path_row_empty(self, board, new_col):
+        """Проверь, свободен ли путь по вертикали под номером new_row доски board."""
         direction = 1 if self.col - new_col < 0 else -1
         for i in range(1, abs(self.col - new_col)):
             if board[self.row][self.col + i * direction] is not None:
@@ -194,11 +243,44 @@ class Rook(Figure):
 
 
 class Queen(Figure):
+    """Класс королевы (ферзя, визиря)."""
     def can_move(self, board, new_row, new_col):
-        bishop_copy_self = copy(self, Bishop)
-        rook_copy_self = copy(self, Rook)
-        return Bishop.can_move(bishop_copy_self, board, new_row, new_col) \
-            or Rook.can_move(rook_copy_self, board, new_row, new_col)
+        if ((self.col - self.row == new_col - new_row) or
+                (self.col + self.row == new_col + new_row) or
+                (self.row == new_row) or
+                (self.col == new_col)):
+            return self.check_path_empty(board, new_row, new_col)
+
+    def check_path_empty(self, board, new_row, new_col):
+        return (self.check_path_col_empty(board, new_row) or
+                self.check_path_row_empty(board, new_col) or
+                self.check_path_diagonal_empty(board, new_row, new_col))
+
+    def check_path_col_empty(self, board, new_row):
+        """Проверь, свободен ли путь по горизонтали под номером new_row доски board."""
+        direction = 1 if self.row - new_row < 0 else -1
+        for i in range(1, abs(self.row - new_row)):
+            if board[self.row + i * direction][self.col] is not None:
+                return False
+        return True
+
+    def check_path_row_empty(self, board, new_col):
+        """Проверь, свободен ли путь по вертикали под номером new_row доски board."""
+        direction = 1 if self.col - new_col < 0 else -1
+        for i in range(1, abs(self.col - new_col)):
+            if board[self.row][self.col + i * direction] is not None:
+                return False
+        return True
+
+    def check_path_diagonal_empty(self, board, new_row, new_col):
+        """Проверь, свободен ли путь по диагонали до координаты new_row, new_col доски board."""
+        direction_row = 1 if new_row - self.row > 0 else -1
+        direction_col = 1 if new_col - self.col > 0 else -1
+        for i in range(1, abs(self.row - new_row)):
+            checked_cor = board[self.row + i * direction_row][self.col + i * direction_col]
+            if checked_cor is not None:
+                return False
+        return True
 
     def char(self):
         return 'Q'
@@ -209,7 +291,12 @@ class Queen(Figure):
 
 
 class King(Figure):
+    """Класс короля (шаха)."""
     def __init__(self, row, col, color: bool):
+        """Расширить инициализацию
+
+        Публичное поле:
+        can_castle - способность ладьи к рокировке, убирается при первом ходе."""
         super(King, self).__init__(row, col, color)
         self.can_castle = True
 
@@ -228,3 +315,11 @@ class King(Figure):
     @staticmethod
     def get_pos_img_in_sp():
         return 1
+
+
+def copy(copy_obj: Figure, cls: type):
+    """Функция копирования copy_obj в cls.
+
+    copy_obj - копируемый объект
+    cls - тип скопированного объекта."""
+    return cls(copy_obj.row, copy_obj.col, copy_obj.color)
